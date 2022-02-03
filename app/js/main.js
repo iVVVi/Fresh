d85057b491237da1a02609de2849336268140766
Victoria Kolesnykova <victoria47774@gmail.com>
2022-02-04 01:14:46 +0200

    header done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-   
+
+    // window.addEventListener('scroll', function(){
+    //     var headerInfo = document.querySelector('.header__info');
+    //     headerInfo.classList.toggle('scroll', window.scrollY > 50);
+    //   });
+
 });
